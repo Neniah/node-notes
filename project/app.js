c1ebece23f9428de66d198ef0225a3c5ed228bd5
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-18 19:39:21 +0100

    body-parser

--- project/app.js
+++ project/app.js
@@ -1,8 +1,11 @@
 var express = require("express");
-
+var bodyParser = require("body-parser");
 var app = express();
 
-app.use(express.static('public'));
+app.use("/public", express.static('public'));
+
+app.use(bodyParser.json()); //application/json
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'jade');
 
@@ -11,7 +14,14 @@ app.get("/", function(req, res){
 });
 
 app.get("/login", function(req, res){
+
   res.render("login");
+});
+
+app.post("/users", function(req, res){
+  console.log("Password: " +  req.body.password);
+  console.log("Email: " +  req.body.email);
+  res.send("We got the data");
 });
 
 app.listen(8080);
