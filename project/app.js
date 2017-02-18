e02b65dcdf59c39b36f7a6becc990ab83ae06eee
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-18 16:38:40 +0100

    Index and login

--- project/app.js
+++ project/app.js
@@ -5,7 +5,11 @@ var app = express();
 app.set('view engine', 'jade');
 
 app.get("/", function(req, res){
-  res.render("index", {hola: "Hola María"});
+  res.render("index");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
 });
 
 app.listen(8080);
