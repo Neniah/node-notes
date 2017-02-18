9dd046f1441600176b802f5d514b02442fea850a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-02-18 20:24:22 +0100

    Mongodb connection

--- project/app.js
+++ project/app.js
@@ -1,6 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+mongoose.connect("mongodb://localhost/photos");
+
+var userSchemaJSON = {
+  email:String,
+  password:String
+};
+
+var user_schema = new Schema(userSchemaJSON);
+
+var User = mongoose.model("User", user_schema);
 
 app.use("/public", express.static('public'));
 
@@ -14,14 +27,20 @@ app.get("/", function(req, res){
 });
 
 app.get("/login", function(req, res){
-
+  User.find(function(err, doc){
+    console.log(doc);
+    res.render("login");
+  });
   res.render("login");
 });
 
 app.post("/users", function(req, res){
-  console.log("Password: " +  req.body.password);
-  console.log("Email: " +  req.body.email);
-  res.send("We got the data");
+  var user = new User({email: req.body.email, password: req.body.password});
+
+  user.save(function(){
+    res.send("The user was saved.");
+  });
+
 });
 
 app.listen(8080);
