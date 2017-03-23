458b19c45ccbe56694bda91d2ee2e6d3fc0b2df7
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-23 13:50:59 +0100

    Cleaning

--- section3/app.js
+++ section3/app.js
@@ -4,20 +4,3 @@ const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
-
-
-var filterArray = _.uniq(['Maria',1, 'Maria', 1, 2, 3, 4]);
-console.log(filterArray);
-
-//console.log(_.isString(true));
-//console.log(_.isString('Maria'));
-
-//var res = notes.addNote();
-//console.log(res);
-
-//var sum = notes.add(3,5);
-//console.log('Result:' , sum);
-
-//var user = os.userInfo();
-
-//fs.appendFile('greetings.txt', `\nHello ${user.username}! You are ${notes.age}`);
