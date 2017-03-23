565e3f34d87042b1a56b7ac89231e8cf184c90f8
Maria Lobillo <maria.lobillo.santos@gmail.com>
2017-03-23 13:30:58 +0100

    Lodash, uniq

--- section3/app.js
+++ section3/app.js
@@ -2,13 +2,21 @@ console.log('Startig app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
 
-var sum = notes.add(3,5);
-console.log('Result:' , sum);
+var filterArray = _.uniq(['Maria',1, 'Maria', 1, 2, 3, 4]);
+console.log(filterArray);
+
+//console.log(_.isString(true));
+//console.log(_.isString('Maria'));
+
+//var res = notes.addNote();
+//console.log(res);
+
+//var sum = notes.add(3,5);
+//console.log('Result:' , sum);
 
 //var user = os.userInfo();
 
